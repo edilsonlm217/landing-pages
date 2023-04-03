90a39050545637d1c2db95489349a0063b188112
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2023-04-03 17:25:56 -0400

    Redirecionamento da rota root para a landing-page-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/lp1',
+    pathMatch: 'full'
+  },
   { path: 'lp1', loadChildren: () => import('./modules/lp1/lp1.module').then(m => m.Lp1Module) }
 ];
 
