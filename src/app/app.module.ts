903b6bbfab96eb3a7b756f057f9213435ade9d4d
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2023-04-15 01:55:47 -0400

    Remover Declaração do CarouselComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CarouselComponent
   ],
   imports: [
     BrowserModule,
