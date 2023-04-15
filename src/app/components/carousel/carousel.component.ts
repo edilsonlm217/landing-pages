a16f168afa1dfcfd188d83efd10c447e4f82bdbd
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2023-04-15 01:59:58 -0400

    Implementação do carousel-component

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
+  slides = [
+    { img: "http://mk-edge.com.br/images/Screenshot_20230310_144646_MK-Edge.jpg" },
+    { img: "http://mk-edge.com.br/images/Screenshot_20230310_144706_MK-Edge.jpg" },
+    { img: "http://mk-edge.com.br/images/Screenshot_20230310_144646_MK-Edge.jpg" },
+    { img: "http://mk-edge.com.br/images/Screenshot_20230310_144706_MK-Edge.jpg" }
+  ];
 
+  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1, infinite: false, };
+
+  slickInit(e: any): void { console.log('slick initialized') }
+  breakpoint(e: any): void { console.log('breakpoint') }
+  afterChange(e: any): void { console.log('afterChange') }
+  beforeChange(e: any): void { console.log('beforeChange') }
 }
