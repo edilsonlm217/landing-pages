8a57c1b02b96a0b412490abe7c2334a74a3c9936
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2023-04-15 01:53:16 -0400

    Declaração do carousel-component

--- src/app/modules/lp1/lp1.module.ts
+++ src/app/modules/lp1/lp1.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { Lp1RoutingModule } from './lp1-routing.module';
 import { Lp1Component } from './lp1.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CarouselComponent } from 'src/app/components/carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
-    Lp1Component
+    Lp1Component,
+    CarouselComponent,
   ],
   imports: [
     CommonModule,
