11856bda174b2149ae022c333f4256e0fb52ada8
Edilson Lima <edilsonlm217@hotmail.com>
2023-04-15 14:04:49 -0400

    Implementação da função contratar

--- src/app/modules/oferta/oferta.component.ts
+++ src/app/modules/oferta/oferta.component.ts
@@ -10,4 +10,8 @@ export class OfertaComponent {
   items = ['Entenda o motivo'];
   expandedIndex = 0;
 
+  contratar(): void {
+    window.location.href = "https://whatsa.me/5511949520936/?t=Quero%20adquirir%20o%20MK-Edge"
+  }
+
 }
